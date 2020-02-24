e1181c80d943026fdd674ffd4c63bd313d58b7b2
karlglans <karl_glans@hotmail.com>
2020-02-24 01:35:55 +0100

    refactoring

--- src/components/ControlPanel/index.jsx
+++ src/components/ControlPanel/index.jsx
@@ -1,32 +1,30 @@
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useLayoutEffect} from 'react';
 
-const ControlPanel = props => {
+const ControlPanel = ({ isAllowingNextStep, currentPlayer, gameOver, handleStep, handleRestart }) => {
   const [allowInput, setAllowInput] = useState(false);
-  const setButtonText = props.isAllowingNextStep ? 'step' : 'wait';
-  const nextPlayer = 1 + props.currentPlayer % 2;
-  const currentPlayer = props.gameOver ? nextPlayer : props.currentPlayer;
-  const bgColor = props.isAllowingNextStep ?
-    currentPlayer === 1? 'red': 'blue' : 
-    currentPlayer === 1? 'red': 'blue';
+  const setButtonText = isAllowingNextStep ? 'step' : 'wait';
+  const nextPlayer = 1 + currentPlayer % 2;
+  const currPlayer = gameOver ? nextPlayer : currentPlayer;
+  const bgColor = currPlayer === 1? 'red': 'blue';
 
-  useEffect( () => {
+  useLayoutEffect( () => {
     // to prevent a second click to get stored when main component stuck on heavy job
-    if(!props.isAllowingNextStep) setAllowInput(false);
+    if(!isAllowingNextStep) setAllowInput(false);
     setTimeout(() => {
-      setAllowInput(props.isAllowingNextStep);
+      setAllowInput(isAllowingNextStep);
     }, 100)
-  }, [props.isAllowingNextStep]);
+  }, [isAllowingNextStep]);
   
   return (
     <div className={!allowInput ? 'animate-flicker' : null}
       style={{ position:'absolute', height: 100, width: 100, right: -110, top: 0, backgroundColor: bgColor }}
     >
-      {!props.gameOver && ( <button style={{marginTop: 20, cursor: 'pointer' }} disabled={!allowInput} 
-        onClick={props.handleStep}> 
+      {!gameOver && ( <button style={{marginTop: 20, cursor: 'pointer' }} disabled={!allowInput} 
+        onClick={handleStep}> 
         {setButtonText} </button>)}
-      {props.gameOver && (<p style={{color:'white'}}>Game Over</p>)}
-      {props.gameOver && ( <button style={{marginTop: 5}} onClick={props.handleRestart}> 
+      {gameOver && (<p style={{color:'white'}}>Game Over</p>)}
+      {gameOver && ( <button style={{marginTop: 5}} onClick={handleRestart}> 
         restart </button>)}
     </div>
   );
